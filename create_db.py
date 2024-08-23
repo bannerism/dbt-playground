34e581cabd4109fad81b04c12475465b4912cc52
Jeffrey Banner <8760445+bannerism@users.noreply.github.com>
2024-08-23 23:57:07 +0000

    init

---
+++ create_db.py
@@ -0,0 +1,18 @@
+import duckdb
+import pandas as pd
+
+DATABASE='data/_nyc_parking_violations.db'
+
+def get_query(sql_location):
+   """read the contents of the sql file"""
+   with open(sql_location, 'r') as f:
+      _sql = f.read()
+    
+   return _sql
+
+sql_query_import_1 = get_query('data/sql_query_import_1.sql')
+sql_query_import_2 = get_query('data/sql_query_import_2.sql')
+
+with duckdb.connect(DATABASE) as con:
+    con.sql(f"{sql_query_import_1}")
+    con.sql(f"{sql_query_import_2}")
